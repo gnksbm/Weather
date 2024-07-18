9b0490ece397978c66e46f028fa0f6e33af1030a
Geonseob Kim <gnksbm@gmail.com>
2024-07-18 19:07:37 +0900

    [Feat] TableView, CollectionView DelegateProxy 구현

---
+++ Weather/Sources/Foundation/Extension/Combine/UICollectionViewDelegateProxy.swift
@@ -0,0 +1,66 @@
+//
+//  UICollectionViewDelegateProxy.swift
+//  Weather
+//
+//  Created by gnksbm on 7/18/24.
+//
+
+import Combine
+import UIKit
+
+extension UICollectionView {
+    var didSelectItemEvent: AnyPublisher<IndexPath, Never> {
+        UICollectionViewDelegateProxy(self).eraseToAnyPublisher()
+    }
+}
+
+struct UICollectionViewDelegateProxy: Publisher {
+    typealias Output = IndexPath
+    typealias Failure = Never
+    
+    let collectionView: UICollectionView
+    
+    init(_ collectionView: UICollectionView) {
+        self.collectionView = collectionView
+    }
+    
+    func receive<S>(
+        subscriber: S
+    ) where S : Subscriber, Never == S.Failure, IndexPath == S.Input {
+        subscriber.receive(
+            subscription: UICollectionViewDelegateSubscrioption(
+                subscriber: subscriber,
+                collectionView: collectionView
+            )
+        )
+    }
+}
+
+class UICollectionViewDelegateSubscrioption<S: Subscriber<IndexPath, Never>>:
+    NSObject, UICollectionViewDelegate, Subscription {
+    var subscriber: S?
+    var demand: Subscribers.Demand = .none
+    
+    init(subscriber: S, collectionView: UICollectionView) {
+        self.subscriber = subscriber
+        super.init()
+        collectionView.delegate = self
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        self.demand += demand
+    }
+    
+    func cancel() {
+        subscriber = nil
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        guard demand > .none else { return }
+        demand -= .max(1)
+        _ = subscriber?.receive(indexPath)
+    }
+}
---
+++ Weather/Sources/Foundation/Extension/Combine/UITableViewDelegateProxy.swift
@@ -0,0 +1,66 @@
+//
+//  UITableViewDelegateProxy.swift
+//  Weather
+//
+//  Created by gnksbm on 7/18/24.
+//
+
+import Combine
+import UIKit
+
+extension UITableView {
+    var didSelectRowEvent: AnyPublisher<IndexPath, Never> {
+        UITableViewDelegateProxy(self).eraseToAnyPublisher()
+    }
+}
+
+struct UITableViewDelegateProxy<TableView: UITableView>: Publisher {
+    typealias Output = IndexPath
+    typealias Failure = Never
+    
+    let tableView: TableView
+    
+    init(_ tableView: TableView) {
+        self.tableView = tableView
+    }
+    
+    func receive<S>(
+        subscriber: S
+    ) where S : Subscriber, Never == S.Failure, IndexPath == S.Input {
+        subscriber.receive(
+            subscription: UITableViewDelegateSubscrioption(
+                subscriber: subscriber,
+                tableView: tableView
+            )
+        )
+    }
+}
+
+class UITableViewDelegateSubscrioption
+<S: Subscriber<IndexPath, Never>, TableView: UITableView>: NSObject, UITableViewDelegate, Subscription {
+    var subscriber: S?
+    var demand: Subscribers.Demand = .none
+    
+    init(subscriber: S, tableView: TableView) {
+        self.subscriber = subscriber
+        super.init()
+        tableView.delegate = self
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        self.demand += demand
+    }
+    
+    func cancel() {
+        subscriber = nil
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        guard demand > .none else { return }
+        demand -= .max(1)
+        _ = subscriber?.receive(indexPath)
+    }
+}
